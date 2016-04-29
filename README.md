b98be2119f59769808587c69cf9df12344d8f569
Dylan Swoope <swoopedj@gmail.com>
2016-04-28 22:28:56 -0500

    initial setup of node server

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # health_inspector
-Restaurant inspection score app using City of Austin's dataset
+Restaurant inspection score app using City of Austin's dataset.
+
+Current server setup with Node.js/Express to be replaced with Django.
--- server/server.js
+++ server/server.js
@@ -1,16 +1,38 @@
 var express = require('express');
-var app = express();
-
-var port = process.env.PORT || 8080;
+var Path = require('path');
 var routes = express.Router();
 
 
-app.get('/', function(request, response){
-  response.sendFile(__dirname + '/index.html');
-});
+//route to index.html
+var assetFolder = Path.resolve(__dirname, '../client/');
+routes.use(express.static(assetFolder));
+
+//endpoints
+// routes.get('/api/*', function(req, res){
+// });
+
+if(process.env.NODE_ENV !== 'test'){
+  routes.get('/*', function(request, response){
+    response.sendFile(assetFolder + '/index.html');
+  });
+
+  //create server
+  var app = express();
+  app.use(require('body-parser').json())
+
+  //main router
+  app.use('/', routes)
+
+  //start server
+  var port = process.env.PORT || 8080;
+  app.listen(port, function(){
+    console.log('listening on port '+ port);
+  });
+} else {
+  //for test env
+  module.exports = routes;
+}
 
 
-app.listen(port, function(){
-	console.log('listening on port '+ port);
-});
+
 
