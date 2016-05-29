735f57f2522f809f114fecd2fde7b5e3f01e5765
Dylan Swoope <swoopedj@gmail.com>
2016-05-29 00:00:39 -0500

    deleted second console.log() that referred to 'this'

--- client/app/controllers/LocationDisplayController.js
+++ client/app/controllers/LocationDisplayController.js
@@ -3,5 +3,4 @@ angular.module('inspector.location', [])
 .controller('LocationDisplayController', function($scope, $http, $routeParams, ResultService) {
   var controller = this;
   this.inspections = ResultService.inspections;
-  console.log(this.inspections);
 });
